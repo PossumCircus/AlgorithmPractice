064d64811c53119f4580d25ea700f2206b937258
PossumCircus <121272786+PossumCircus@users.noreply.github.com>
2023-04-01 14:57:37 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+    
+    var str = my_string.replaceAll('a', '')
+    .replaceAll('e', '')
+    .replaceAll('i', '')
+    .replaceAll('o', '')
+    .replaceAll('u', '');
+    
+    return str
+    
+    
+    
+    
+    
+}
