ec8ef9a40f8fea2dc23e980ff33ab1c83c226eca
김종현 <plmij1501@gmail.com>
2023-10-22 09:53:15 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,11 +1,3 @@
 function solution(my_string) {
-    const a= [...my_string].map( val => val.match(/[0-9]/gi)).reduce( (init, curr) => init+curr)
-    
-    if(a === 0){
-        return 0
-    } else {
-        return a.split('null').reduce( (init, curr) => Number(init)+Number(curr),0)
-    }
-    
-    // .split('null').reduce( (init, curr) => Number(init)+Number(curr),0)
+   return my_string.split(/\D+/).reduce( (acc, curr) => Number(acc)+Number(curr), 0)
 }
