8e12ad42e19d11a43b09eca89db41cad2e8697d7
김종현 <plmij1501@gmail.com>
2024-02-02 17:52:54 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,19 +1,3 @@
-function solution(A, B) {
-    const arr = A.split('')
-    
-    function pushing(A){
-     const pop = arr.pop()
-     arr.unshift(pop)
-    }
-    
-    let i = 0;
-    
-    while(i<A.length){
-        if(arr.join('') === B){
-            break;
-        }
-        pushing(A)
-        i++
-    }
-    return (i === A.length && arr.join('') !== B ) ? -1 : i
-}
+
+
+let solution=(a,b)=>(b+b).indexOf(a)
