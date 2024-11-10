be2bc00a426e7ea532e257ef9a9c5fbaceaa21f9
김종현 <plmij1501@gmail.com>
2024-11-10 11:21:44 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/README.md
+++ 프로그래머스/1/76501. 음양 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 음양 더하기 - 76501 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 1.05 ms
+메모리: 33.4 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 10일 11:16:38
+2024년 11월 10일 11:21:43
 
 ### 문제 설명
 
--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,9 +1,13 @@
-function solution(absolutes, signs) {
-    
-    const mapped = absolutes.map( (v, i) => {
-        return signs[i] === false ? v[i] = v-v*2 : v[i] = v
-    }).reduce( (acc, cur) => acc+cur, 0)
-    
-    return mapped
+// function solution(absolutes, signs) {
+//     const mapped = absolutes.map( (v, i) => {
+//         return !signs[i] ? v[i] = v-v*2 : v[i] = v
+//     }).reduce( (acc, cur) => acc+cur, 0)
+//     return mapped
+// }
 
-}
+function solution(absolutes, signs) {
+    const answer = absolutes.reduce( ( acc, cur, i) => {
+        return !signs[i] ? acc + (cur*-1) : acc + cur
+    },0)
+        return answer
+    }
